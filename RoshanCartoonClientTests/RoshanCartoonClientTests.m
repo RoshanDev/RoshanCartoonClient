b7031e281431688fac8542909236f0944e4c14a5
pibao2013 <1083254162@qq.com>
2014-05-22 01:18:08 +0800

    Initial Commit

---
+++ RoshanCartoonClientTests/RoshanCartoonClientTests.m
@@ -0,0 +1,34 @@
+//
+//  RoshanCartoonClientTests.m
+//  RoshanCartoonClientTests
+//
+//  Created by 方辉 on 14-5-22.
+//  Copyright (c) 2014年 方辉. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RoshanCartoonClientTests : XCTestCase
+
+@end
+
+@implementation RoshanCartoonClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
